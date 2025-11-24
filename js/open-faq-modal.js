3d7b9b51523a8a28979ba2b081a087a79b02f4e0
simon <semyon.dudin@inbox.ru>
2025-11-24 14:40:54 +0300

    готовый сайт

--- js/open-faq-modal.js
+++ js/open-faq-modal.js
@@ -2,8 +2,9 @@ window.addEventListener('DOMContentLoaded', () => {
 	const openBtn = document.querySelector('.question');
 	const modal = document.getElementById('modalFaqOverlay');
 	const closeBtn = document.getElementById('modalFaqClose');
+	const sendBtn = document.querySelector('.modal-faq-btn'); // ← кнопка "Отправить"
 
-	if (!openBtn || !modal || !closeBtn) return;
+	if (!openBtn || !modal || !closeBtn || !sendBtn) return;
 
 	openBtn.addEventListener('click', () => {
 		modal.classList.add('active');
@@ -13,9 +14,15 @@ window.addEventListener('DOMContentLoaded', () => {
 		modal.classList.remove('active');
 	});
 
+	sendBtn.addEventListener('click', (e) => {
+		e.preventDefault();      // чтобы форма не перезагружала страницу
+		modal.classList.remove('active'); // закрыть модалку
+	});
+
 	modal.addEventListener('click', (e) => {
 		if (e.target === modal) {
 			modal.classList.remove('active');
 		}
 	});
 });
+
